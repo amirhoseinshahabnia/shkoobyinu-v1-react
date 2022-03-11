c501be1c32dd0d17cdca00c39ebaa81678321711
Amirhossein Shahabnia <amirhosein.shahabnia@gmail.com>
2022-03-10 22:57:42 -0800

    adds homepage desktop only till timeline

--- app/src/components/nav.jsx
+++ app/src/components/nav.jsx
@@ -3,47 +3,84 @@ import { Link } from 'react-router-dom';
 import Login from '../login';
 import { useAccount } from 'wagmi';
 import { ClipboardIcon } from '@heroicons/react/outline';
+import styled from 'styled-components';
+import { ReactComponent as MainLogo } from '../assets/img/logo-dark.svg';
+import ChakraSemiBold from '../assets/fonts/ChakraPetch-Bold.ttf';
+
+const StyledNav = styled.header`
+  height: 65px;
+  border-bottom: 1px solid #fff;
+  background-color: #000;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  justify-content: space-between;
+
+  .logo-link {
+    width: 15%;
+  }
+
+  #heaedr-logo {
+    display: block;
+    max-width: 250px;
+    cursor: pointer;
+  }
+`;
+
+const StyledLinks = styled.ul`
+  font-family: 'Chakra Bold';
+  list-style: none;
+  padding: 0;
+  display: flex;
+  width: 65%;
+  justify-content: space-around;
+
+  li {
+    font-size: 1.125em;
+    text-transform: uppercase;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 20%;
+  max-width: 231px;
+`;
 
 const Nav = () => {
-  const [{ data: accountData }, disconnect] = useAccount();
+  // const [{ data: accountData }, disconnect] = useAccount();
   return (
-    <div className="flex-grow flex justify-end mt-4 mr-12 -mb-20">
-      {accountData ? (
-        <div className="flex items-center">
-          <div className="flex mr-4 text-white gap-4">
-            <Link className="mr-2 " to="/invest">
-              Invest
-            </Link>
-
-            <div className="flex items-center">
-              <p className="">{`${[...accountData.address]
-                .splice(0, 6)
-                .join('')}...${[...accountData.address]
-                .splice(37)
-                .join('')}`}</p>
-              <ClipboardIcon
-                onClick={() =>
-                  navigator.clipboard.writeText(accountData.address)
-                }
-                className="h-4 w-4 -mt-2 text-slate-200 cursor-pointer"
-              ></ClipboardIcon>
-            </div>
-          </div>
-          <button
-            className=" py-2 px-4 rounded-lg bg-white mr-4"
-            onClick={() => {
-              disconnect();
-            }}
-          >
-            Disconnect
-          </button>
-        </div>
-      ) : (
-        <Link className=" py-2 px-4 rounded-lg bg-white" to={'/'}>
-          Connect
-        </Link>
-      )}
-    </div>
+    <StyledNav>
+      <Link to="/" className="logo-link">
+        <MainLogo id="heaedr-logo" className="opacity" />
+      </Link>
+      <StyledLinks>
+        <li>
+          <a className="opacity white" href="#">
+            Roadmap
+          </a>
+        </li>
+        <li>
+          <a className="opacity white external" href="#">
+            Litepaper
+          </a>
+        </li>
+        <li>
+          <a className="opacity white" href="#">
+            Staking
+          </a>
+        </li>
+        <li>
+          <a className="opacity white external" href="#">
+            Staking Guide
+          </a>
+        </li>
+      </StyledLinks>
+      <StyledButton className="primary-bg main-btn">Buy Shkooby</StyledButton>
+    </StyledNav>
   );
 };
 
