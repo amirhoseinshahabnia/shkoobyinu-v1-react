815e7dc2c1ed3533e1557824bb51768fb3317124
Amirhossein Shahabnia <amirhosein.shahabnia@gmail.com>
2022-03-11 16:47:41 -0800

    adds mobile header + adjusts some of the global font-sizings

--- app/src/components/nav.jsx
+++ app/src/components/nav.jsx
@@ -1,8 +1,12 @@
-import { Buffer } from 'buffer';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
+import SideDrawer from './sideDrawer';
+import Backdrop from './backdrop';
 import { ReactComponent as MainLogo } from '../assets/img/logo-dark.svg';
-import ChakraSemiBold from '../assets/fonts/ChakraPetch-Bold.ttf';
+import mobileLogo from '../assets/img/heaedr-logo-mobile@2x.png';
+import hamburgerIcon from '../assets/img/hamburger-icon@2x.png';
 
 const StyledNav = styled.header`
   height: 65px;
@@ -22,6 +26,55 @@ const StyledNav = styled.header`
     max-width: 250px;
     cursor: pointer;
   }
+
+  #heaedr-logo-mobile {
+    display: block;
+    height: 80px;
+  }
+
+  .ham-ctn {
+    flex-basis: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  #hamburger-menu {
+    width: 75px;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 768px) {
+    height: 100px;
+
+    .logo-link {
+      flex-basis: 100%;
+    }
+  }
+
+  @media screen and (max-width: 1000px) {
+    padding: 0 10px;
+  }
+
+  @media screen and (max-width: 480px) {
+    padding: 0 10px;
+
+    .logo-link {
+      flex-basis: 80%;
+    }
+
+    .ham-ctn {
+      flex-basis: 80%;
+    }
+
+    #hamburger-menu {
+      width: 60px;
+    }
+
+    #heaedr-logo-mobile {
+      display: block;
+      height: 60px;
+    }
+  }
 `;
 
 const StyledLinks = styled.ul`
@@ -40,19 +93,108 @@ const StyledLinks = styled.ul`
   a {
     text-decoration: none;
   }
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+
+  @media screen and (min-width: 769px) and (max-width: 809px) {
+    width: 56%;
+  }
+
+  @media screen and (min-width: 810px) and (max-width: 999px) {
+    width: 60%;
+  }
 `;
 
 const StyledButton = styled.button`
   width: 20%;
   max-width: 231px;
+
+  @media screen and (max-width: 768px) {
+    width: 230px;
+    flex-basis: 100%;
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 200px;
+    flex-basis: 120%;
+  }
+
+  @media screen and (max-width: 390px) {
+    flex-basis: 130%;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 999px) {
+    width: 175px;
+    max-width: 175px;
+    flex-basis: 100%;
+  }
+
+  @media screen and (min-width: 1000px) and (max-width: 1200px) {
+    width: 190px;
+    max-width: 190px;
+    flex-basis: 100%;
+  }
 `;
 
 const Nav = () => {
   // const [{ data: accountData }, disconnect] = useAccount();
+
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
+  const isTablet = useMediaQuery({
+    query: '(max-width: 768px)',
+  });
+
   return (
     <StyledNav>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <ul className="mobile-links">
+          <li>
+            <a className="opacity white" href="#frontTime">
+              Roadmap
+            </a>
+          </li>
+          <li className="external-mobile">
+            <a
+              className="opacity white "
+              href="https://www.docdroid.net/7LeGzhU/shkooby-inu-litepaper-deck-1-pdf#page=3"
+              target="_blank"
+            >
+              Litepaper
+            </a>
+          </li>
+          <li>
+            <Link className="opacity white" to="/staking">
+              Staking
+            </Link>
+            <ul>
+              <li className="wallet white">Dashboard</li>
+            </ul>
+          </li>
+          <li className="external-mobile">
+            <a className="opacity white" href="#">
+              Staking Guide
+            </a>
+          </li>
+        </ul>
+      </SideDrawer>
       <Link to="/" className="logo-link">
-        <MainLogo id="heaedr-logo" className="opacity" />
+        {isTablet ? (
+          <img id="heaedr-logo-mobile" className="opacity" src={mobileLogo} />
+        ) : (
+          <MainLogo id="heaedr-logo" className="opacity" />
+        )}
       </Link>
       <StyledLinks>
         <li>
@@ -61,14 +203,18 @@ const Nav = () => {
           </a>
         </li>
         <li>
-          <a className="opacity white external" href="https://www.docdroid.net/7LeGzhU/shkooby-inu-litepaper-deck-1-pdf#page=3" target="_blank">
+          <a
+            className="opacity white external"
+            href="https://www.docdroid.net/7LeGzhU/shkooby-inu-litepaper-deck-1-pdf#page=3"
+            target="_blank"
+          >
             Litepaper
           </a>
         </li>
         <li>
-        <Link className="opacity white" to="/staking">
+          <Link className="opacity white" to="/staking">
             Staking
-        </Link>
+          </Link>
         </li>
         <li>
           <a className="opacity white external" href="#">
@@ -77,6 +223,17 @@ const Nav = () => {
         </li>
       </StyledLinks>
       <StyledButton className="primary-bg main-btn">Buy Shkooby</StyledButton>
+      {isTablet ? (
+        <div className="ham-ctn">
+          <img
+            src={hamburgerIcon}
+            alt="hamburger menu"
+            onClick={openDrawerHandler}
+            id="hamburger-menu"
+            className="opacity"
+          />
+        </div>
+      ) : null}
     </StyledNav>
   );
 };
